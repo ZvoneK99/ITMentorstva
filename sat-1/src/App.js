c04dcfc3a84801afb06a72c62164264f0f3e9a3c
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-01-31 17:29:27 +0100

    Add Bootstrap for styling and enhance product management UI
    - Integrated Bootstrap CSS for improved styling.
    - Updated Products component to include input fields for adding new products.
    - Enhanced search functionality with user feedback.
    - Cleaned up code formatting for better readability.

--- sat-1/src/App.js
+++ sat-1/src/App.js
@@ -1,19 +1,20 @@
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Products from './Components/Products';
 import { useState } from 'react';
 
-
-
 function App() {
- let [tax, setTax] = useState(0)
-
+  let [tax, setTax] = useState(0)
 
   return (
-    <>
-      
-      <input type="text" placeholder='Enter tax' onInput={(e) => setTax(e.target.value)}/>
-      <Products tax={tax}/>
-    </>    
+    <div className='container'>
+
+      <Products tax={tax} />
+      <div className='mt-2'>
+        <input className='form-control' type="text" placeholder='Enter tax' onInput={(e) => setTax(e.target.value)} />
+      </div>
+    </div>
+
   );
 }
 
--- sat-1/src/Components/Products.js
+++ sat-1/src/Components/Products.js
@@ -4,69 +4,101 @@ import { useState } from 'react';
 function Products(props) {
 
     let [products, setProducts] = useState({
-    'iPhone 14': 1000,
-     'iPhone 15': 1250,
-      'Samsung S23S': 1100,
-});
+        'iPhone 14': 1000,
+        'iPhone 15': 1250,
+        'Samsung S23S': 1100,
+    });
 
-/*
-    1. Input za unos imena proizvoda - newProductName, setNewProductName
-    2. input za unos cijene - newProductPrice, setNewProductPrice
-    3. button za kreiranje proizvoda - onClick: addProducts -> newProductName, newProductPrice push products
-*/
+    /*
+        1. Input za unos imena proizvoda - newProductName, setNewProductName
+        2. input za unos cijene - newProductPrice, setNewProductPrice
+        3. button za kreiranje proizvoda - onClick: addProducts -> newProductName, newProductPrice push products
+    */
     let [newProductName, setNewProductName] = useState();
     let [newProductPrice, setNewProductPrice] = useState();
+    let [result, setResult] = useState();
+    function addProduct() {
 
-function addProduct() {
-        
-    //console.log(newProductName,newProductPrice);
-    if (!newProductName) {
-        console.log("Niste unijeli ime!");
-        return;
+        //console.log(newProductName,newProductPrice);
+        if (!newProductName) {
+            console.log("Niste unijeli ime!");
+            return;
+        }
+        if (!newProductPrice) {
+            console.log("Niste unijeli cijenu!");
+            return;
+        }
+
+        console.log("Sve ste unijeli!");
+
+        setProducts({
+            ...products,
+            [newProductName]: parseInt(newProductPrice)
+        });
     }
-    if (!newProductPrice) {
-        console.log("Niste unijeli cijenu!");
-        return;
-    }
-
-    console.log("Sve ste unijeli!");
-
-    setProducts({
-        ...products,
-        [newProductName]: parseInt(newProductPrice)
-    });
-}
 
 
 
     function searchProduct(e) {
+        let searchTerm = e.target.value.toLowerCase(); //Ovo korisnik pretrazuje i prebacimo ga u lower Case
+        console.log("Pretraga za pojam " + searchTerm);
+        let exist = false;
+        let keys = Object.keys(products); //Napraviti array od kljuceva poroducts-a
+        for (let i = 0; i < keys.length; i++) {
+            const keysLower = keys[i].toLowerCase();
+            if (keysLower === searchTerm) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            console.log("Pronašli smo proizvod!");
+            setResult("Pronašli smo proizvod!");
+        }
+        else {
+            console.log("Nismo pronašli proizvod!");
+            setResult("Nismo pronašli proizvod!");
 
-        const searchValue = e.target.value;;
-        const search = products.hasOwnProperty(searchValue);
-        console.log(search);
-
+        }
     }
+
 
     return (
         <>
-    <button onClick={() => setProducts({})}>Delete products</button>
 
-           {Object.entries(products).map(([phone, price]) => (
-    <p key={phone}>
-        {phone}, price: {price}, with tax: {CalculateTax(price, props.tax)}
-    </p>
-))}
 
-    <div style={{margin: 20}}>
-    <input type='text' placeholder='Unesi ime proizvoda' onInput={(e) => setNewProductName(e.target.value)}></input>
-        <input type='number' placeholder='Unesi cijenu proizvoda' onInput={(e) => setNewProductPrice(e.target.value)}></input>
-        <button onClick={addProduct}>Add new products!</button>
-        <div>
+            <div className='d-flex justify-content-center'>
+                {Object.entries(products).map(([phone, price]) => (
+                    <div className='m-2'>
+                        <h5>{phone}</h5>
+                        <p>Price: ${price}, price with tax ${CalculateTax(price, props.tax)}</p>
+                    </div>
+
+                ))
+                }
+            </div>
+
+
             <div>
-        <input placeholder='Pretrazi objekt' onInput={searchProduct}/>
-        </div>
-        </div>
-    </div>
+                <hr />
+                <h3>Dodavanje proizvoda</h3>
+                <div className='mt-2'>
+                    <input className='form-control' type='text' placeholder='Unesi ime proizvoda' onInput={(e) => setNewProductName(e.target.value)}></input>
+                </div>
+                <div className='mt-2'>
+                    <input className='form-control' type='number' placeholder='Unesi cijenu proizvoda' onInput={(e) => setNewProductPrice(e.target.value)}></input>
+                </div>
+                <button className='btn btn-primary mt-2' onClick={addProduct}>Add new products!</button><br />
+                <button className='btn btn-danger mt-2' onClick={() => setProducts({})}>Delete products</button>
+
+                <hr />
+                <div>
+                    <div>
+                        <p>{result}</p>
+                        <input className='form-control' placeholder='Pretrazi objekt' onInput={searchProduct} />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
@@ -76,7 +108,7 @@ function addProduct() {
 
 
 function CalculateTax(price, tax) {
-    return price +(price*tax / 100);
+    return price + (price * tax / 100);
 }
 
 
