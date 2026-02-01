4055f78982eaff1f7cf616abdb789f584ac02733
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-02-01 16:56:46 +0100

    Added table for storing results on screen

--- domaci-4/src/Components/City.js
+++ domaci-4/src/Components/City.js
@@ -6,7 +6,7 @@ function City() {
     let [city, setCity] = useState("");
     let [country, setCountry] = useState("");
     let [temperature, setTemperature] = useState("");
-
+    let [cities, setCities] = useState([]);
     function EnterCity(e) {
         const city = e.target.value;
         //console.log(city);
@@ -25,6 +25,14 @@ function City() {
         setTemperature(temperature);
     }
 
+    function AddToList() {
+        setCities([
+            ...cities,
+            { city, country, temperature }
+        ]);
+
+    }
+
     return (
         <div className="d-flex justify-content-center align-items-center min-vh-100 flex-column bg-dark">
             <div className="mt-2 border w-50 rounded card mx-auto bg-primary-subtle">
@@ -38,9 +46,10 @@ function City() {
                 <div className="mx-auto">
                     <input className="form-control m-3  rounded" type="number" placeholder="Enter temperature" onInput={(j) => EnterTemperature(j)} />
                 </div>
+                <button className="btn btn-primary w-25 mx-auto" onClick={AddToList}>Add</button>
             </div>
             <div className="mt-2 border w-50 rounded card mx-auto bg-white">
-                <h5 className="text-center">Enter data results</h5>
+                <h5 className="text-center">Current data result</h5>
                 <div className="mx-auto">
                     <p className="fw-semibold">City: <span className="text-primary">{city}</span></p>
                     <hr />
@@ -54,7 +63,30 @@ function City() {
                     <hr />
                 </div>
             </div>
-        </div>
+            <div className="mt-2 border w-50 rounded card mx-auto bg-danger">
+                <h5 className="text-center">Results</h5>
+                <table className="">
+                    <thead>
+                        <tr>
+                            <th>City</th>
+                            <th>Country</th>
+                            <th>Temperature</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {cities.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.city}</td>
+                                <td>{item.country}</td>
+                                <td>{item.temperature}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+                </table>
+            </div>
+        </div >
     );
 }
 export default City;
