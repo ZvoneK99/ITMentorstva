74cc4d76f15fdf9d2b61f5e8853b9e4c9f262a9a
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-02-02 21:04:37 +0100

    Finished exercise

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
 
-function App() {
+const VIDEOS = [
+  {
+    id: 1,
+    title: "How to learn React",
+    url: "https://www.youtube.com/watch?v=s2skans2dP4&t=50s",
+    cover: "https://i.ytimg.com/vi/s2skans2dP4/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLB_EAd36LXIk8Vl-p2qkZ9y_OTJiQ"
+  },
+  {
+    id: 2,
+    title: "How to learn CSS",
+    url: "https://www.youtube.com/watch?v=Z4pCqK-V_Wo",
+    cover: "https://i.ytimg.com/vi/Z4pCqK-V_Wo/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCf_uoqGukhMFc-j7qUQKEicLudUQ"
+  },
+]
+
+
+
+const App = () => {
+
   return (
-    <h1>You Tube</h1>
+    <>
+      {VIDEOS.map(video => {
+        return <div>
+          <a href={video.url} target='_blank'>
+            <img src={video.cover} />
+            <h3>{video.title}</h3>
+          </a>
+        </div>
+      })}
+    </>
   );
+
 }
+
 
 export default App;
