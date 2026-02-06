56fa21756849228d367d782430527cb80866edba
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-02-06 23:16:23 +0100

    Install bootstrap

--- domaci-5/src/App.js
+++ domaci-5/src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home';
 import Video from './Pages/Video';
--- domaci-5/src/Components/AllVideos.js
+++ domaci-5/src/Components/AllVideos.js
@@ -4,10 +4,12 @@ const AllVideos = () => {
     return (
         <>
             {VIDEOS.map(video => {
-                return <div key={video.id}>
-                    <h1>{video.Title}</h1>
-                    <h3>{video.url}</h3>
-                    <iframe src={video.url} title={video.title} />
+                return <div className='d-block m-auto bg-primary'>
+                    <div className='d-block w-50 mb-5 m-auto border bg-secondary' key={video.id}>
+                        <h1>Video tite: {video.Title}</h1>
+                        <h3>{video.url}</h3>
+                        <iframe src={video.url} title={video.Title} />
+                    </div>
                 </div>
             })}
         </>
