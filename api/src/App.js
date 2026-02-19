207594620d75ddc285a20a6cc28e87d1ff9531cb
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-02-19 23:05:05 +0100

    Testing api key and url

--- api/src/App.js
+++ api/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+console.log(process.env.REACT_APP_OMDBAPI_KEY, process.env.REACT_APP_OMDBAPI_URL);
+
 function App() {
   return (
     <div className="App">
