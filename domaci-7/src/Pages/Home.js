78b16b17a8d09e730fdcf40fbb7a6e94cb23dd21
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-02-23 19:24:33 +0100

    Added title, input and some css

---
+++ domaci-7/src/Pages/Home.js
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div className="movie-search-container d-flex justify-content-center align-items-center flex-column">
+            <h1>Movie search</h1>
+            <input type="text" placeholder="Enter the name of the movie" />
+        </div>
+    );
+}
+
+export default Home;    
