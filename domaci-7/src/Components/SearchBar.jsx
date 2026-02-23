67942867d9775124d21997afb779d69ecbec5733
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-02-23 20:25:02 +0100

    Final commit

--- domaci-7/src/Components/SearchBar.jsx
+++ domaci-7/src/Components/SearchBar.jsx
@@ -5,20 +5,24 @@ const SearchBar = () => {
     const apiKey = process.env.REACT_APP_API_KEY;
     const apiUrl = process.env.REACT_APP_API_URL;
     const [movieName, setMovieName] = useState("");
-    const searchMovieByName = (name) => {
-        //console.log(name.target.value);
-        setMovieName(name.target.value);
+
+    const searchMovieByTitle = (name) => {
+        // console.log(movieName);
+        axios.get(`${apiUrl}/?t=${movieName}&apikey=${apiKey}`)
+            .then(response => console.log(response.data));
     }
 
-    axios.get(`${apiUrl}/?t=${movieName}&apikey=${apiKey}`)
-        .then(response => console.log(response.data));
+
 
 
 
     return (
         <div className="movie-search-container d-flex justify-content-center align-items-center flex-column">
             <h1>Movie search</h1>
-            <input type="text" placeholder="Enter the name of the movie" onChange={searchMovieByName} />
+            <form>
+                <input type="text" placeholder="Enter the name of the movie" onInput={e => setMovieName(e.target.value)} />
+                <button type="button" onClick={searchMovieByTitle}>Search!</button>
+            </form>
         </div>
     );
 }
