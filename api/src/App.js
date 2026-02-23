d3cd55afad172156889d3afe7e0fb0c2f9afdad4
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-02-23 20:58:44 +0100

    added component searchbar

--- api/src/App.js
+++ api/src/App.js
@@ -1,38 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-
-//console.log(process.env.REACT_APP_OMDBAPI_KEY, process.env.REACT_APP_OMDBAPI_URL);
-
-// fetch("http://www.omdbapi.com/?t=Terminator&apikey=85c1766e")
-//   .then(response => response.json())
-//   .then(jsonResponse => console.log(jsonResponse))
-
-const URL = process.env.REACT_APP_OMDBAPI_URL;
-const apiKey = process.env.REACT_APP_OMDBAPI_KEY
-
-axios.get(`${URL}/?t=Terminator&apikey=${apiKey}`)
-  .then(response => console.log(response.data))
-  .catch(error => console.log(error));
+import SearchBar from './Components/SearchBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SearchBar />
   );
 }
 
---
+++ api/src/Components/SearchBar.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+
+const SearchBar = () => {
+    const apiKey = process.env.REACT_APP_OMDBAPI_KEY;
+    const apiUrl = process.env.REACT_APP_OMDBAPI_URL;
+    const [movieName, setMovieName] = useState("");
+
+    const searchMovieByTitle = (name) => {
+        // console.log(movieName);
+        axios.get(`${apiUrl}/?t=${movieName}&apikey=${apiKey}`)
+            .then(response => console.log(response.data));
+    }
+
+
+
+
+
+    return (
+        <div className="movie-search-container d-flex justify-content-center align-items-center flex-column">
+            <h1>Movie search</h1>
+            <form>
+                <input type="text" placeholder="Enter the name of the movie" onInput={e => setMovieName(e.target.value)} />
+                <button type="button" onClick={searchMovieByTitle}>Search!</button>
+            </form>
+        </div>
+    );
+}
+export default SearchBar;
