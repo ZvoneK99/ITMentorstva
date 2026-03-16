f02504b1179f41a4564dfd48b94c3c8428b47797
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-03-16 20:52:32 +0100

    Added SearchProvider and MoviesContext

--- api/src/App.js
+++ api/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Movies from './Components/Movies';
 import Navigation from './Templates/Snippets/Navigation';
 import Footer from './Templates/Snippets/Footer';
 import Search from './Components/Search';
-import { useContext } from 'react';
 
 function App() {
   return (
