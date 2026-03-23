b8b6e0f8b33fa61f83314667047ebb33278ed76e
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-03-23 21:07:59 +0100

    Added functionality to change both currencies

--- use-context/src/App.js
+++ use-context/src/App.js
@@ -7,9 +7,11 @@ import CurrencyList from "./Currency.json";
 export const CurrencyContext = createContext("USD");
 export const AmountContext = createContext(0);
 export const ConvertedAmountContext = createContext(0);
+export const InitialCurrencyContext = createContext("BAM")
 
 function App() {
-
+  const [initialCurrency, setInitialCurrency] = useState("BAM")
+  const [initialValue, setInitialValue] = useState(1)
   const [currency, setCurrency] = useState("USD");
   const [value, setValue] = useState(0.5918);
   const [amount, setAmount] = useState(0);
@@ -20,8 +22,14 @@ function App() {
   }
 
   useEffect(() => {
-    setConvertedAmount(amount * value);
-  }, [amount, value]);
+    setConvertedAmount(amount * (value / initialValue));
+  }, [amount, value, initialValue]);
+
+  const updateInitialCurrency = (valuta) => {
+    const selected = CurrencyList.find(c => c.valuta === valuta)
+    setInitialCurrency(selected.valuta);
+    setInitialValue(selected.value);
+  }
 
   const updateCurrency = (valuta) => {
     const selected = CurrencyList.find(c => c.valuta === valuta)
@@ -35,6 +43,15 @@ function App() {
         <h1 className='mb-3'>Currency calculator</h1>
         <input onInput={(e) => updateAmount(e.target.value)} />
         <div>
+          <label>Choose initial currency: </label>
+          <select onChange={(e) => updateInitialCurrency(e.target.value)}>
+
+            {CurrencyList.map(currency => {
+              return (<option key={currency.id} value={currency.valuta}>{currency.valuta}</option>)
+            })}
+          </select>
+        </div>
+        <div>
           <label>Choose currency: </label>
           <select onChange={(e) => updateCurrency(e.target.value)}>
 
@@ -47,7 +64,9 @@ function App() {
         <CurrencyContext.Provider value={{ currency, updateCurrency }}>
           <AmountContext.Provider value={{ amount, updateAmount }}>
             <ConvertedAmountContext.Provider value={{ convertedAmount, setConvertedAmount }}>
-              <Payment />
+              <InitialCurrencyContext.Provider value={{ initialCurrency, updateInitialCurrency }}>
+                <Payment />
+              </InitialCurrencyContext.Provider>
             </ConvertedAmountContext.Provider>
           </AmountContext.Provider>
         </CurrencyContext.Provider>
--- use-context/src/Components/PayPal.jsx
+++ use-context/src/Components/PayPal.jsx
@@ -1,15 +1,15 @@
 import { useContext } from "react";
-import { AmountContext, ConvertedAmountContext, CurrencyContext } from "../App";
+import { AmountContext, ConvertedAmountContext, CurrencyContext, InitialCurrencyContext } from "../App";
 
 const PayPal = () => {
-
+    const { initialCurrency } = useContext(InitialCurrencyContext);
     const { currency } = useContext(CurrencyContext);
     const { amount } = useContext(AmountContext);
     const { convertedAmount } = useContext(ConvertedAmountContext)
     // console.log(currency);
     // console.log(amount);
     return (<>
-        <h1>{amount} BAM = {convertedAmount.toFixed(2)} {currency}</h1>
+        <h1>{amount} {initialCurrency} = {convertedAmount.toFixed(2)} {currency}</h1>
     </>);
 }
 
