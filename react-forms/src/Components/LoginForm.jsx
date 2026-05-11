7bde0f86ded4d36e02643a5c1af3daf42cdd48e4
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-05-11 19:28:51 +0200

    Cleaning code and add pasword validate

--- react-forms/src/Components/LoginForm.jsx
+++ react-forms/src/Components/LoginForm.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,29 +5,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const LoginForm = () => {
     const { register, handleSubmit, formState: { errors } } = useForm()
 
-    const onSubmit = data => console.log(data);
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-
+    const formSubmitted = data => console.log(data);
 
 
     return (
         <div className='d-flex justify-content-center align-items-center flex-column min-vh-100 mb-2'>
-            <form className='d-flex flex-column gap-2' onSubmit={handleSubmit(onSubmit)}>
+            <form className='d-flex flex-column gap-2' onSubmit={handleSubmit(formSubmitted)}>
 
                 <input {...register("firstname", { required: true, maxLength: 20 })} type="text" placeholder="Username" />
                 {errors.firstname?.type === 'required' && <span>Username is required!</span>}
                 {errors.firstname?.type === 'maxLength' && <span>Username is too long!</span>}
 
 
-                <input {...register("password", { required: true, minLength: 6, maxLength: 20 })} type="password" placeholder="Password" />
+                <input {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    validate: value => value.trim() !== ''
+                })} type="password" placeholder="Password" />
                 {errors.password?.type === 'minLength' && <span>Password is too short!</span>}
                 {errors.password?.type === 'maxLength' && <span>Password is too long!</span>}
                 {errors.password?.type === 'required' && <span>Password is required!</span>}
+                {errors.password?.type === 'validate' && <span>Password cannot hold empty space!</span>}
                 <button type="submit">Login</button>
-                <p>{passwordError}</p>
             </form>
         </div>
     )
