b9540a61a7e387caa1580dfe95f2aaa7b1de60cc
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-05-11 18:22:44 +0200

    Added form

---
+++ react-forms/src/Components/LoginForm.jsx
@@ -0,0 +1,13 @@
+
+
+const LoginForm = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
+    )
+}
+
+export default LoginForm;
