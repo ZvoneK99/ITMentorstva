78bc32f558145cbbea5b1319e65d42ff3b169ac0
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-05-11 18:30:36 +0200

    Added checking password

--- react-forms/src/Components/LoginForm.jsx
+++ react-forms/src/Components/LoginForm.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 
 
 const LoginForm = () => {
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password.trim() === '') {
+            alert('Password cannot be empty');
+            return;
+        } else {
+            setPassword(password);
+        }
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
             <button type="submit">Login</button>
         </form>
     )
