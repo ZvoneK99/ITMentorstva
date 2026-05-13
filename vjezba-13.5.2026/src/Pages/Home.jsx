894d98f65b28cd3009f39659fb1106588dc1a64a
Zvone Kožul <zvone.kozull1999@gmail.com>
2026-05-13 21:02:07 +0200

    Testing form using useForm

--- vjezba-13.5.2026/src/Pages/Home.jsx
+++ vjezba-13.5.2026/src/Pages/Home.jsx
@@ -1,27 +1,40 @@
 import gradovi from '../data/Gradovi.json';
+import { useForm } from 'react-hook-form';
+
 
 const Home = () => {
 
+    const validCities = gradovi.map((g) => g.name.toLowerCase());
 
-    const ispisGrada = (grad) => {
-        grad = grad.toLowerCase();
-        const pronađen = gradovi.find((g) => g.name.toLowerCase() === grad);
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-        if (pronađen) {
-            console.log("Grad pronađen: ", pronađen);
-        } else {
-            console.log("Grad nije pronađen: ", grad);
-        }
+
+    // const ispisGrada = (grad) => {
+    //     grad = grad.toLowerCase();
+    //     const pronađen = gradovi.find((g) => g.name.toLowerCase() === grad);
+
+    //     if (pronađen) {
+    //         console.log("Grad pronađen: ", pronađen);
+    //     } else {
+    //         console.log("Grad nije pronađen: ", grad);
+    //     }
+    // };
+
+    const onFormSubmit = (data) => {
+        console.log(data);
     };
 
+    const isValidCityName = (name) => {
+        return validCities.includes(name.toLowerCase());
+    };
 
     return (
         <>
             {/* Hero Section */}
             <div className="bg-gradient text-white text-center py-5" style={{ background: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)' }}>
                 <div className="container">
-                    <form>
-                        <input type="text" className="form-control form-control-lg mb-3" placeholder="Unesite naziv grada..." onChange={(e) => ispisGrada(e.target.value)} />
+                    <form onSubmit={handleSubmit((onFormSubmit))}>
+                        <input {...register("grad", { validate: isValidCityName })} type="text" className="form-control form-control-lg mb-3" placeholder="Unesite naziv grada..." />
                     </form>
                     <h1 className="display-4 fw-bold mb-3">Dobrodošli na Home!</h1>
                     <p className="lead mb-4">Ovo je moderna React aplikacija sa Bootstrap stilom</p>
